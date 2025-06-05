e18b4aac6bdb2c22a01ad4ef7084abf6efe53bda
leticia oliveira <83239872+antonycabral@users.noreply.github.com>
2025-06-05 14:05:50 -0300

    passo 1 - estrutura inicial do projeto

---
+++ src/test/java/com/leticia/agendamento/AgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leticia.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

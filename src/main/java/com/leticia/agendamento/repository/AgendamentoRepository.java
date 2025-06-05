6aa94bb86e353f4ab1101ea4810e0b09facde657
leticia oliveira <83239872+antonycabral@users.noreply.github.com>
2025-06-05 14:21:54 -0300

    passo 3 - repositório AgendamentoRepository criado

---
+++ src/main/java/com/leticia/agendamento/repository/AgendamentoRepository.java
@@ -0,0 +1,9 @@
+package com.leticia.agendamento.repository;
+
+import com.leticia.agendamento.Model.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+    // Aqui podemos criar consultas personalizadas depois, se precisar
+}
+
